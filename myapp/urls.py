200677c7a7cd94ed13f28e704d14189bb9a55b50
SININA <SININA@Sininas-MacBook-Air.local>
2024-07-02 20:47:09 +0530

    region sent.

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,12 +1,18 @@
 
-from django.urls import path
+from django.urls import path,include
+from rest_framework.routers import DefaultRouter
 from .views import UserRegistrationView, OTPVerificationView, CustomLoginView, ResendOTPView
+from .views import RegionViewSet
+
+router = DefaultRouter()
+router.register(r'regions', RegionViewSet, basename='region')
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('auth/login/', CustomLoginView.as_view(), name='custom_login'), 
     path('register/', UserRegistrationView.as_view(), name='register'),
     path('verify-otp/', OTPVerificationView.as_view(), name='verify-otp'),
     path('resend-otp/', ResendOTPView.as_view(), name='resend-otp'),
+    # path('regions/', RegionView.as_view(), name='regions'),
 
 ]
-
--- myapp/views.py
+++ myapp/views.py
@@ -64,8 +64,20 @@ class CustomLoginView(APIView):
         }, status=status.HTTP_200_OK)
     
 class RegionViewSet(viewsets.ReadOnlyModelViewSet):
+    permission_classes = [AllowAny]
     queryset = Region.objects.all()
     serializer_class = RegionSerializer  
+
+
+# class RegionView(generics.GenericAPIView):
+#     queryset = Region.objects.all()
+#     serializer_class = RegionSerializer
+
+#     def get(self, request, *args, **kwargs):
+#         queryset = self.get_queryset()
+#         serializer = self.get_serializer(queryset, many=True)
+#         return Response(serializer.data)
+
 
 
 
