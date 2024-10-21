7db5425ca48528a108a9bd41fceb050536656782
SININA <SININA@Sininas-MacBook-Air.local>
2024-10-21 16:43:14 +0530

    template added

--- realty_b/urls.py
+++ realty_b/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-
 # from dj_rest_auth.registration.views import SocialAccountListView, SocialAccountDisconnectView
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 
 
@@ -12,6 +12,8 @@ urlpatterns = [
     path('api/', include('myapp.urls')),
     path('payments/', include('subscriptions.urls')),
     path('notifications/', include('notification_chat.urls')),
+    path('', TemplateView.as_view(template_name='index.html'), name='home'),  # Serve your frontend app here
+
 
     # path('api/auth/', include('dj_rest_auth.urls')),  
     # path('api/auth/registration/', include('dj_rest_auth.registration.urls')),
